0bf8144b0dfc07b2857cd415a29d66bf72b04f45
githubnicholas <nicholas.masse@monithq.ca>
2019-08-14 22:39:28 -0400

    final

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,14 @@
+// Déclaration des plats et des prix dans des tableaux
+
 const entrees = ['Choisir...', 'Salade', 'Escargots', 'Soupe'];
 const repas = ['Choisir...', 'Spaghetti', 'Lasagne', 'Boeuf'];
 const prixEntrees = [0, 5, 7, 3];
 const prixRepas = [0, 10, 15, 20];
 
-
+// fonction pour charger les plats de la page, invoquée au chargement du BODY (ONLOAD)
 function chargerPlats() {
+
+    // les entrées
     let taille = entrees.length;
     let elem = document.getElementById('entrees');
     let choix = 'Choisir...';
@@ -19,12 +23,13 @@ function chargerPlats() {
         elem.appendChild(node);
     }
 
+    // les plats
     taille = repas.length;
     elem = document.getElementById('repas');
-    for (let i = 0;i < taille;i++){
+    for (let i = 0; i < taille; i++) {
         let node = document.createElement('LI');
         let textnode = document.createTextNode(repas[i]);
-        if (entrees[i] == choix){
+        if (entrees[i] == choix) {
             node.setAttribute("data-selected", true);
         }
         node.appendChild(textnode);
@@ -33,6 +38,7 @@ function chargerPlats() {
     }
 }
 
+//fonction pour changer l'image de l'entrée, invoquée si un changement survient (ONCHANGE)
 function chargerEntree() {
 
     let elem = document.getElementById('sample4');
@@ -50,31 +56,33 @@ function chargerEntree() {
             break;
         default:
             conteneur.style.backgroundImage = 'url(images/entrees.jpg)';
-        break;
+            break;
     }
 }
 
+//fonction pour changer l'image du plat, invoquée si un changement survient (ONCHANGE)
 function chargerRepas() {
     let elem = document.getElementById('sample5');
     let conteneur = document.getElementById('selectdiv2');
     let choix = elem.value;
-    switch(choix){
+    switch (choix) {
         case 'Spaghetti':
             conteneur.style.backgroundImage = 'url(images/spaghetti.jpg)';
             break;
-            case 'Lasagne':
+        case 'Lasagne':
             conteneur.style.backgroundImage = 'url(images/lasagna.jpg)';
             break;
-            case 'Boeuf':
+        case 'Boeuf':
             conteneur.style.backgroundImage = 'url(images/beef.jpg)';
             break;
-            default:
+        default:
             conteneur.style.backgroundImage = 'url(images/burger.jpg)';
             break;
 
     }
 }
 
+// fonction pour mettre à jour la facture (le tableau), chargée dès qu'un changement survient (ONCHANGE)
 function updateTable() {
     let index = 0;
     let elem1 = document.getElementById('sample4');
@@ -83,8 +91,10 @@ function updateTable() {
     let prixEntree = document.getElementById('prixEntree');
     let nomRepas = document.getElementById('nomRepas');
     let prixPlat = document.getElementById('prixRepas');
+
+    // trouve quel élément est sélectionné pour l'entrée
     entrees.forEach(element => {
-        if (element == elem1.value){
+        if (element == elem1.value) {
             index = entrees.indexOf(element);
             nomEntree.textContent = entrees[index];
             prixEntree.textContent = '$ ' + prixEntrees[index];
@@ -93,8 +103,9 @@ function updateTable() {
 
     prixEntree = prixEntrees[index];
 
+    // trouve quel élément est sélectionné pour le repas principal
     repas.forEach(element => {
-        if (element == elem2.value){
+        if (element == elem2.value) {
             index = repas.indexOf(element);
             nomRepas.textContent = repas[index];
             prixPlat.textContent = '$ ' + prixRepas[index];
@@ -106,6 +117,7 @@ function updateTable() {
     let taxes = sous_total * 0.14975;
     let total = sous_total + taxes;
 
+    //met à jour la table
     document.getElementById('sous-total').textContent = '$ ' + sous_total;
     document.getElementById('taxes').textContent = '$ ' + taxes.toFixed(2);
     document.getElementById('total').textContent = '$ ' + total.toFixed(2);
